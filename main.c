18f3f2c513ed30606785260dc03e513557df413a
Lola Pires Pinto <lolapinto@live.fr>
2021-05-02 16:23:28 +0200

    debut permutation

--- main.c
+++ main.c
@@ -9,9 +9,7 @@ int main(int argc, char *argv[]){
 
     printf("DONNÉES QUE NOUS POSSÉDONS POUR CHIFFRER : \n(message, clee) : (0x%s,0x%s)",argv[1],argv[2]);
 
-    //cadencement des clées
     chiffrement(argv[1],argv[2]);
 
-    
     exit(0);
 }
--- present24.h
+++ present24.h
@@ -3,5 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
-int hexa_to_dec(char *mot);
+int hexa_to_dec(char *mot); //Fonction pour convertir un héxa en décimale
+int permutation(int etat); //Fonction pour la permutation
+int substitution(int etat); //Fonction pour la substitution
 void chiffrement(char *message, char *clee); //Fonction pour le chiffrement
