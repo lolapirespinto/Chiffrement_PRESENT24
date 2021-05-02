096f66a65105a13111596153514e2362278027b8
Lola Pires Pinto <lolapinto@live.fr>
2021-05-02 19:39:49 +0200

    ajout cadencement

--- present24.h
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-
-int hexa_to_dec(char *mot); //Fonction pour convertir un héxa en décimale
-int permutation(int etat); //Fonction pour la permutation
-int substitution(int etat); //Fonction pour la substitution
-void chiffrement(char *message, char *clee); //Fonction pour le chiffrement
